1d846ef7a65f3d13c434000aa443b9e2d9deb8f3
mehditajik <39027849+mehditajik@users.noreply.github.com>
2023-04-21 10:51:07 +0330

    Add files via upload

---
+++ forward.sh
@@ -0,0 +1,11 @@
+read -p 'Enter your main server IP like(1.1.1.1): ' IP
+echo "The main server IP is $IP"
+sudo apt  install iptables iptables-persistent
+sudo sysctl net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -p tcp --match multiport --dports  4444 -j MASQUERADE 
+sudo iptables -t nat -A PREROUTING -p tcp --match multiport --dports  7766 -j DNAT --to-destination $IP
+sudo iptables -t nat -A POSTROUTING -p udp -j MASQUERADE 
+sudo iptables -t nat -A PREROUTING -p udp -j DNAT --to-destination $IP
+
+sudo mkdir -p /etc/iptables/ 
+sudo iptables-save | sudo tee /etc/iptables/rules.v4
